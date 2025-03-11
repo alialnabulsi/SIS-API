1d7db51f8bb9f1f97c69127f341c5ba49b57cd6e
alialnabulsi <ali.alnabulsi@std.balamand.edu.lb>
2025-03-11 22:26:33 +0200

    Intilaized the server in index .js and modified the db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,22 @@
 const database= require("mariadb");
-require('dotenv').config;
+require('dotenv').config();
 
 const pool = database.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    connectionLimit: 10});
+    connectionLimit: 10
+});
 
-    module.exports = pool;
+const connectDB = async () => {
+    try{
+        const conn = await pool.getConnection();
+        console.log("✅ Database connected successfully");
+        conn.release(); //Back the connection for pool
+    }catch(err){
+        console.error("❌ Database connection failed:", err.message);
+        process.exit(1);
+    }
+};
+    module.exports = {pool, connectDB};
---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+//Import Routes
+
+
+
+//Database Coneection
+const {connectDB} = require('./config/db');
+
+const app = express();
+
+// Connect to Database
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+
+
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => 
+    {
+    console.log(`🚀 Server running on port ${PORT}`)
+    }
+);
