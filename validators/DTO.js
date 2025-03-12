02d4897df790cd63a13be93d709f06c7cadb3998
alialnabulsi <ali.alnabulsi@std.balamand.edu.lb>
2025-03-12 02:10:48 +0200

    Added validators for other routes

--- validators/DTO.js
+++ validators/DTO.js
@@ -1,5 +1,6 @@
 const {body, param, validationResult} = require('express-validator');
- 
+
+//createLocation case
  const validateLocation = [
      body('city')
          .isString()
@@ -28,7 +29,61 @@ const {body, param, validationResult} = require('express-validator');
          next();
      }
  ];
+
+ //getLocation case
+
+const validateCityParam = [
+    param('city')
+        .isString()
+        .withMessage('City must be a string')
+        .notEmpty()
+        .withMessage('City is required')
+        .isLength({ min: 3, max: 255 })
+        .withMessage('City name must be between 3 and 255 characters'),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+];
+
+
+//updateLocation case
+ const validateLocationUpdate = [
+    param('city')
+        .isString()
+        .withMessage('City must be a string')
+        .notEmpty()
+        .withMessage('City is required')
+        .isLength({ min: 3, max: 255 })
+        .withMessage('City name must be between 3 and 255 characters'),
+
+    body('zipCode')
+        .optional()
+        .isString()
+        .withMessage('Zip Code must be a string')
+        .isLength({ max: 255 })
+        .withMessage('Zip Code must not be longer than 255 characters'),
+
+    body('address')
+        .optional()
+        .isString()
+        .withMessage('Address must be a string')
+        .isLength({ min: 5, max: 255 })
+        .withMessage('Address must be between 5 and 255 characters'),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+];
  
- 
- module.exports = validateLocation;
+
+ module.exports = {validateLocation,validateCityParam,validateLocationUpdate};
  
